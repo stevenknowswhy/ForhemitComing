ca576134b1ede820dade20605d7cea42f0db7f29
Stefano94120 <stephenobamastokes@gmail.com>
2026-03-15 19:10:20 -0700

    Add The Exit Crisis page with Problem/Solution, Crises, and Solutions sections

---
+++ app/the-exit-crisis/_components/sections/index.ts
@@ -0,0 +1,3 @@
+export { ProblemSolutionSection } from "./ProblemSolutionSection";
+export { CrisesSection } from "./CrisesSection";
+export { SolutionsSection } from "./SolutionsSection";
---
+++ app/the-exit-crisis/page.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import Link from "next/link";
+import { useGlobalScrollReveal } from "@/hooks/useIntersectionObserver";
+
+// Import styles from about page since we're using the same CSS classes
+import "../about/about-page.css";
+
+// Section components
+import {
+  ProblemSolutionSection,
+  CrisesSection,
+  SolutionsSection,
+} from "./_components/sections";
+
+export default function TheExitCrisisPage() {
+  // Initialize scroll reveal animations
+  useGlobalScrollReveal();
+
+  return (
+    <div className="about-wrapper">
+      <div className="about-background"></div>
+
+      {/* Logo Header */}
+      <header className="about-logo-header">
+        <Link href="/" className="about-logo-link">
+          <span className="about-logo-text">Forhemit</span>
+          <span className="about-logo-underline"></span>
+        </Link>
+      </header>
+
+      <main className="about-main">
+        <ProblemSolutionSection />
+        <CrisesSection />
+        <SolutionsSection />
+      </main>
+    </div>
+  );
+}
