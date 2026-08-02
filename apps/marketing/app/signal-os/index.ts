64848730086a6eca2c15c6d7267129c63df2fe58
Stefano94120 <stephenobamastokes@gmail.com>
2026-08-01 21:41:39 -0700

    add Signal OS feature barrel

---
+++ apps/marketing/app/signal-os/index.ts
@@ -0,0 +1,5 @@
+export { SignalOsPage } from "./SignalOsPage";
+export { SignalOsPage as default } from "./SignalOsPage";
+export { canonicalUrl } from "./constants";
+export { buildProductJsonLd, isFoundingPricingActive } from "./lib/pricing";
+export type { SignalOsPageProps, SignalOsTier } from "./types";
